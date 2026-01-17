627b16b6970cfd93a8208eeb29e563c26ed04c2b
Prodipta Nayeb <nayebprodipta@gmail.com>
2026-01-17 15:00:58 +0530

    Update stack implementation for dynamic size and input
    Refactor stack implementation to allow dynamic size and improve input handling.

--- Data Structure/stack_pop.c
+++ Data Structure/stack_pop.c
@@ -21,7 +21,7 @@ int isfull(stack *pt)
 {
     if(pt->top ==pt->size-1)
     {
-        
+        return 1;
     }
     else
     {
@@ -31,38 +31,37 @@ int isfull(stack *pt)
 void main()
 {
     stack *s=(stack*)malloc(sizeof(stack));
-    int item=0,i=0;
-    s->size= 5;
+    int item=0,i=0,*pr, length=0;
+    s->size= 100;
     s->top= -1;
     s->arr= (int *)malloc(s->size* sizeof(int));
+   
+    printf("Enter the size of the stack:\n");
+    scanf("%d",&length);    
+     pr=(int *)malloc(length*sizeof(int));
     printf("Push elements in the stack:\n");
-    for(i=0;i<s->size;i++)
+    for(i=0;i<length;i++)
     {
-        scanf("%d",&s->arr[i]);
-        item= s->arr[i];
+        scanf("%d",&pr[i]);
+        item= pr[i];
         s->top++;
     }
+    printf("Stack after entering elements:\n");
+    for(i=s->top;i>=0;i--)
+    {
+        printf("|_%d_|\n",pr[i]);
+    }
+        printf("stack overflows\n");
+        printf("POPing the last elements:\n");
+        item= pr[s->top];
+        s->top--;
+        for(i=s->top;i>=0;i--)
+        {
+            printf("|_%d_|\n",pr[i]);
+        }
     if(isempty(s))
     {
         printf("stack is empty");
     }
-    else
-    {
-        printf("Stack is not empty\n");
-        for(i=s->top;i>=0;i--)
-        {
-            printf("|_%d_|\n",s->arr[i]);
-        }
-    }
-    if(isfull(s))
-    {
-        printf("stack overflows\n");
-        printf("POPing the last elements:\n");
-        item= s->arr[s->top];
-        s->top--;
-        for(i=s->top;i>=0;i--)
-        {
-            printf("|_%d_|\n",s->arr[i]);
-        }
-    }
+    
 }
