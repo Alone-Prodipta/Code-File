812118c8c38558ee00ddb8513c89e661964fd748
Prodipta Nayeb <nayebprodipta@gmail.com>
2026-01-25 12:14:45 +0000

    written some on traversing and displayig of the linked list

---
+++ Data Structure/linked_list_deletion.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct list
+{
+    int data;
+    struct list* next;
+}node;
+
+void main()
+{
+    node* head;
+    node* dt;
+    node* tri;
+    head= (node* )malloc(sizeof(node));
+    dt =(node* )malloc(sizeof(node));
+    tri =(node* )malloc(sizeof(node));
+    head->data= 52;
+    head->next= dt;
+    
+    dt->data= 82;
+    dt->next= tri;
+
+    tri->data= 94;
+    tri->next= NULL;
+
+    while(head != NULL)
+    {
+        printf("Elemnts: %d\n",head->data);
+        head= head->next;
+    }
+}
