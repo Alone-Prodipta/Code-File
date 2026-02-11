e4d032e35211b8e2b44d8b1803cd873db1dbffb5
Prodipta Nayeb <nayebprodipta@gmail.com>
2026-02-11 12:44:59 +0530

    linked list reversal

--- Data Structure/tempCodeRunnerFile.c
+++ Data Structure/tempCodeRunnerFile.c
@@ -1,1 +1,45 @@
-free(str);
+#include<stdio.h>
+#include<stdlib.h>
+int i=0;
+typedef struct xy
+{
+    int data;
+    struct xy *next; 
+}node;
+node *temp,*p;
+void input_node()
+{
+    node *head;
+    int ele=0;
+    i=1;
+    printf("Enter the elements in the node");
+    scanf("%d",&ele);
+    temp=(int *)malloc(sizeof(int));
+    temp->data=ele;
+    temp->next=NULL;
+    p=temp;
+}
+void main()
+{
+    int n=0;
+    i=2;
+    node *pt=(node *)malloc(sizeof(node));
+    input_node();
+    printf("Enter the total number of nodes");
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        node *head;
+        int ele=0;
+        i=1;
+        printf("Enter the elements in the node");
+        scanf("%d",&ele);
+        temp=(int *)malloc(sizeof(int));
+        temp->data=ele;
+        temp->next=NULL;
+        p->next= temp;
+        p= p->next;
+    }
+    
+
+}
