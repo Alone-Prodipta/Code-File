16157fc2a62021c1ca8c9e26997814f23268acc2
Prodipta Nayeb <nayebprodipta@gmail.com>
2026-03-05 20:06:25 +0530

    modified the file

--- Java/pn_13.java
+++ Java/pn_13.java
@@ -5,27 +5,26 @@ class pn_13
     public static void main(String args[])
     {
         Scanner my= new Scanner(System.in);
-        int ar[]= {40,50,12,80,99,42,60};
-        int i=0,j=0,min=0,temp=0;
-        for(i=0;i<(ar.length)-1;i++)
+        int ar[]= {41,45,80,12,60,77};
+        int min=0,temp=0;
+        for(int i=0;i<ar.length-1;i++)
         {
-            min= i;
-            for(j= i+1;j<ar.length;i++)
+            min=i;
+            for(int j=i+1;i<ar.length;i++)
             {
                 if(ar[min]>ar[j])
                 {
                     min= j;
                 }
                 temp= ar[j];
-                ar[j]= ar[j+1];
-                ar[j+1]=temp;
+                ar[j+1]=ar[j];
+                ar[j]= temp;
             }
         }
-         System.out.println("Printing the sorted array");
-            for(i=0;i<ar.length;i++)
-            {
-                System.out.print(ar[i]+" ");
-            }
-       
+        System.out.println("Printing the list");
+        for(int i=0;i<ar.length;i++)
+        {
+            System.out.println(ar[i]);
+        }
     }
 }
