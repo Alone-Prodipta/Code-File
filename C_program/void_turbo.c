471d99ac72e7744754bec0b50aa4819ee441fa76
Prodipta Nayeb <nayebprodipta@gmail.com>
2026-03-15 20:17:40 +0530

    modified the previous code and added the rest of the months of the calender

--- C_program/void_turbo.c
+++ C_program/void_turbo.c
@@ -1,160 +1,221 @@
-#include<stdio.h>
-#include<stdlib.h>
-int date_31(int c,int data[5][7])
-{
-    int i,j;
-    for(i=1;i<=5;i++)
-    {
-        for(j=1;j<=7;j++)
-        {
-            if(c<10)
-            {
-                printf(" 0%d  ",c);
-            }
-            else
-            {
-                printf(" %d  ",c);
-            }
-            data[i-1][j-1] = c;
-            c++;
-            if(c>31)
-            {
-                printf("\n");
-                return;
-            }
-        }
-        printf("\n");
-    }
-}
-int date_30(int c,int data[5][7])
-{
-    int i,j;
-    for(i=1;i<=5;i++)
-    {
-        for(j=1;j<=7;j++)
-        {
-            if(c<10)
-            {
-                printf(" 0%d  ",c);
-            }
-            else
-            {
-                printf(" %d  ",c);
-            }
-            data[i-1][j-1] = c;
-            c++;
-            if(c>30)
-            {
-                break;
-            }
-        }
-        printf("\n");
-    }
-}
-void janu(int data[7][5])
-{
-    int i,j,c=1;
-    date_31(c,data);
-    
-}
-void april(int c,int data[7][5])
-{
-    int i,j;
-    date_30(c,data);
-}
-void jan(int c,int data[7][5])
-{
-    int i,j;
-    date_31(c,data);
-    
-}
-void feb(int data[7][5])
-{
-    int i,j,c=1;
-    for(int k=1;k<=4;k++)
-    {
-        printf("  0%d ",c);
-        c++;
-    }
-    printf("\n");
-    for(i=1;i<=5;i++)
-    {
-        for(j=1;j<=7;j++)
-        {
-            if(c<10)
-            {
-                printf(" 0%d  ",c);
-            }
-            else
-            {
-                printf(" %d  ",c);
-            }
-            data[i-1][j-1] = c;
-            c++;
-            if(c>28)
-            {
-                printf("\n");
-               return;
-            }
-        }
-        printf("\n");
-    }
-}
-typedef struct list
-{
-    int data[7][5];
-    struct list* next;
-}node;
-void main()
-{
-    int c=1;
-    node* january;
-    node* february;
-    january= (node*)malloc(sizeof(node));
-    february= (node*)malloc(sizeof(node));
-    printf("......2023......\n");
-    switch(1)
-    {
-        case 1:
-        {
-            printf("     january    \n");
-            printf(" sun mon tues wedn thur frid  sat\n");
-                janu(january->data);
-        }
-        case 2:
-        {
-            printf("     february    \n");
-            printf(" sun mon tues wedn thur frid  sat\n");
-            printf("              ");
-                feb(february->data);
-        }
-        case 3:
-        {
-            printf("     March    \n");
-            printf(" sun mon tues wedn thur frid  sat\n");
-            printf("              ");
-            for(int k=1;k<=4;k++)
-            {
-                printf("  0%d ",c);
-                c++;
-            }
-            printf("\n");
-                jan(c,january->data);
-        }
-        case 4:
-        {
-            c=1;
-            printf("     April    \n");
-            printf(" sun mon tues wedn thur frid  sat\n");
-            printf("                             ");
-            for(int k=1;k<=1;k++)
-            {
-                printf("  0%d ",c);
-                c++;
-            }
-            printf("\n");
-                april(c,january->data);
+#include <stdio.h>
 
+// Print a month with 31 days
+int date_31(int c, int start_day, int data[6][7]) 
+{
+    int i, j;
+    for (j = 0; j < start_day; j++) 
+    {
+        printf("     ");
+    }
+    for (i = 0; i < 6; i++) 
+    {
+        for (j = start_day; j < 7; j++) 
+        {
+            if (c < 10) printf(" 0%d  ", c);
+            else printf(" %d  ", c);
+            data[i][j] = c;
+            c++;
+            if (c > 31) 
+            {
+                printf("\n");
+                return (j + 1) % 7;
+            }
+        }
+        printf("\n");
+        start_day = 0;
+    }
+    return 0;
+}
+
+// Print a month with 30 days
+int date_30(int c, int start_day, int data[6][7]) 
+{
+    int i, j;
+    for (j = 0; j < start_day; j++) 
+    {
+        printf("     ");
+    }
+    for (i = 0; i < 6; i++) 
+    {
+        for (j = start_day; j < 7; j++) 
+        {
+            if (c < 10) printf(" 0%d  ", c);
+            else printf(" %d  ", c);
+            data[i][j] = c;
+            c++;
+            if (c > 30) {
+                printf("\n");
+                return (j + 1) % 7;
+            }
+        }
+        printf("\n");
+        start_day = 0;
+    }
+    return 0;
+}
+
+// February (non-leap year, 28 days)
+int feb(int c, int start_day, int data[6][7]) {
+    int i, j;
+    for (j = 0; j < start_day; j++) {
+        printf("     ");
+    }
+    for (i = 0; i < 5; i++) {
+        for (j = start_day; j < 7; j++) {
+            if (c < 10) printf(" 0%d  ", c);
+            else printf(" %d  ", c);
+            data[i][j] = c;
+            c++;
+            if (c > 28) {
+                printf("\n");
+                return (j + 1) % 7;
+            }
+        }
+        printf("\n");
+        start_day = 0;
+    }
+    return 0;
+}
+
+typedef struct list {
+    int data[6][7];
+} node;
+
+// Print the full year
+void print_2023() {
+    int c, start_day;
+    node january, february, march, april, may, june, july, august, september, october, november, december;
+
+    printf("......2023......\n");
+
+    c = 1;
+    start_day = 0; // January 1, 2023 was Sunday
+
+    for (int i = 1; i <= 12; i++) {
+        if (i == 1)      printf("     January    \n");
+        else if (i == 2) printf("     February   \n");
+        else if (i == 3) printf("     March      \n");
+        else if (i == 4) printf("     April      \n");
+        else if (i == 5) printf("     May        \n");
+        else if (i == 6) printf("     June       \n");
+        else if (i == 7) printf("     July       \n");
+        else if (i == 8) printf("     August     \n");
+        else if (i == 9) printf("     September  \n");
+        else if (i == 10) printf("     October    \n");
+        else if (i == 11) printf("     November   \n");
+        else if (i == 12) printf("     December   \n");
+
+        printf(" sun mon tues wedn thur frid  sat\n");
+
+        switch (i) {
+            case 1:  
+                start_day = date_31(c, start_day, january.data); 
+                break;
+            case 2:  
+                start_day = feb(c, start_day, february.data); 
+                break;
+            case 3:  
+                start_day = date_31(c, start_day, march.data); 
+                break;
+            case 4:  
+                start_day = date_30(c, start_day, april.data); 
+                break;
+            case 5:  
+                start_day = date_31(c, start_day, may.data); 
+                break;
+            case 6:  
+                start_day = date_30(c, start_day, june.data); 
+                break;
+            case 7:  
+                start_day = date_31(c, start_day, july.data); 
+                break;
+            case 8:  
+                start_day = date_31(c, start_day, august.data); 
+                break;
+            case 9:  
+                start_day = date_30(c, start_day, september.data); 
+                break;
+            case 10: 
+                start_day = date_31(c, start_day, october.data); 
+                break;
+            case 11: 
+                start_day = date_30(c, start_day, november.data); 
+                break;
+            case 12: 
+                start_day = date_31(c, start_day, december.data); 
         }
     }
+}
+void print_2024() {
+    int c, start_day;
+    node january, february, march, april, may, june, july, august, september, october, november, december;
+
+    printf("......2024......\n");
+
+    c = 1;
+    start_day = 1; // January 1, 2024 will be Monday
+
+    for (int i = 1; i <= 12; i++) 
+    {
+        if (i == 1)      printf("     January    \n");
+        else if (i == 2) printf("     February   \n");
+        else if (i == 3) printf("     March      \n");
+        else if (i == 4) printf("     April      \n");
+        else if (i == 5) printf("     May        \n");
+        else if (i == 6) printf("     June       \n");
+        else if (i == 7) printf("     July       \n");
+        else if (i == 8) printf("     August     \n");
+        else if (i == 9) printf("     September  \n");
+        else if (i == 10) printf("     October    \n");
+        else if (i == 11) printf("     November   \n");
+        else if (i == 12) printf("     December   \n");
+
+        printf(" sun mon tues wedn thur frid  sat\n");
+
+        switch (i) 
+        {
+            case 1:  
+                start_day = date_31(c, start_day, january.data); 
+                break;
+            case 2:  
+                start_day = feb(c, start_day, february.data); 
+                break;
+            case 3:  
+                start_day = date_31(c, start_day, march.data); 
+                break;
+            case 4:  
+                start_day = date_30(c, start_day, april.data); 
+                break;
+            case 5:  
+                start_day = date_31(c, start_day, may.data); 
+                break;
+            case 6:  
+                start_day = date_30(c, start_day, june.data); 
+                break;
+            case 7:  
+                start_day = date_31(c, start_day, july.data); 
+                break;
+            case 8:  
+                start_day = date_31(c, start_day, august.data); 
+                break;  
+            case 9:
+                start_day = date_30(c, start_day, september.data); 
+                break;
+            case 10: 
+                start_day = date_31(c, start_day, october.data);
+                break;
+            case 11:
+                start_day = date_30(c, start_day, november.data); 
+                break;
+            case 12:
+                start_day = date_31(c, start_day, december.data);
+        }
+    }
+}
+int main() 
+{
+    //print_2023();
+    print_2024();
+    return 0;
 }
