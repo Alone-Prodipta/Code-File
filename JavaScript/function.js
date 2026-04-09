1cdda8d0607780822c23cba38481055208bbc766
Prodipta Nayeb <nayebprodipta@gmail.com>
2026-04-09 13:57:05 +0530

    made the changes

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -9,30 +9,65 @@ prev.addEventListener("click",function(){
     document.querySelector("slide").prepend(items[items.length-1]);
 });*/
 let slide= document.querySelectorAll(".item");
-let body= document.querySelector("body");
-select.forEach(function(item)
+let body= document.querySelector(".container");
+slide.forEach(function(item)
 {
     item.addEventListener("click",function(e)
     {
         if(e.target.id== "red")
         {
             body.style.backgroundColor= e.target.id;
+            body.style.top= "0";
+            body.style.left= "0";
+            body.style.transform= "translate(0,0)";
+            body.style.borderRadius= "0";
+            body.style.width= "100%";
+            body.style.height= "100%";
+            body.style.visibility= "visible";
         }
         if(e.target.id== "pink")
         {
             body.style.backgroundColor= e.target.id;
+            body.style.top= "0";
+            body.style.left= "0";
+            body.style.transform= "translate(0,0)";
+            body.style.borderRadius= "0";
+            body.style.width= "100%";
+            body.style.height= "100%";
+            body.style.visibility= "visible";
         }
         if(e.target.id== "orange")
         {
             body.style.backgroundColor= e.target.id;
+            body.style.top= "0";
+            body.style.left= "0";
+            body.style.transform= "translate(0,0)";
+            body.style.borderRadius= "0";
+            body.style.width= "100%";
+            body.style.height= "100%";
+            body.style.visibility= "visible";
         }
         if(e.target.id== "cyan")
         {
-            body.style.backgroundColor= e.target.id;
+           body.style.backgroundColor= e.target.id;
+            body.style.top= "0";
+            body.style.left= "0";
+            body.style.transform= "translate(0,0)";
+            body.style.borderRadius= "0";
+            body.style.width= "100%";
+            body.style.height= "100%";
+            body.style.visibility= "visible";
         }
         if(e.target.id== "yellow")
         {
             body.style.backgroundColor= e.target.id;
+            body.style.top= "0";
+            body.style.left= "0";
+            body.style.transform= "translate(0,0)";
+            body.style.borderRadius= "0";
+            body.style.width= "100%";
+            body.style.height= "100%";
+            body.style.visibility= "visible";
         }        
     });
 });
