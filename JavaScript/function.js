50bc51a0b2aa6f5bf12b8187ad3844ef27f971f8
Prodipta Nayeb <nayebprodipta@gmail.com>
2026-04-11 11:53:38 +0530

    remove the comments

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -2,13 +2,13 @@ const track = document.querySelector('.slide-track');
 const btnNext = document.querySelector('#forward');
 const btnPrev = document.querySelector('#backward');
 
-// Move the first element to the end of the container
+
 const shiftNext = () => {
     const items = document.querySelectorAll('.slide-card');
     track.appendChild(items[0]);
 };
 
-// Move the last element to the beginning of the container
+
 const shiftPrev = () => {
     const items = document.querySelectorAll('.slide-card');
     track.prepend(items[items.length - 1]);
