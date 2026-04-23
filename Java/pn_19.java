daaa4e7e376e97ce329e66806d523b287a97d89f
Prodipta <nayebprodipta@gmail.com>
2026-04-23 21:02:01 +0530

    created one more basic program

---
+++ Java/pn_19.java
@@ -0,0 +1,10 @@
+class pn_19 
+{
+    public static void main(String[] args) 
+    {
+        int n=7, a=0; 
+        a= n*49/n+35/n;
+        System.out.println("The final result of 'n*49/n+35/n' is :");
+        System.out.print(a);
+    }
+}
