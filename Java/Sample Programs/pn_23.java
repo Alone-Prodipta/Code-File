be8bfc147323f4d948bfd7f6d4b66d984c31b2da
Prodipta <nayebprodipta@gmail.com>
2026-07-10 20:18:07 +0530

    empty commit

---
+++ Java/Sample Programs/pn_23.java
@@ -0,0 +1,10 @@
+class pn_23
+{
+    public static void main(String[] args)
+    {
+        Scanner my= new Scanner(System.in);
+        int ar[]= new int[7];
+        ar[]= [45,89,55,33,19,92,44];
+
+    }
+}
