b2721b13e3ead33fe3a78ad078cd912752c4a80a
Prodipta <nayebprodipta@gmail.com>
2026-07-14 11:24:48 +0530

    removed unnessesary variables

--- Mathematical Programming/Eulers.c
+++ Mathematical Programming/Eulers.c
@@ -3,7 +3,7 @@
 void main()
 {
     int n=0,k=0;
-    float h = 0.01, x0 = 0.0, y0 = 1.0, x_end = 0.02, x[n+1], y[n+1], f=0.00; 
+    float h = 0.01, x_end = 0.02, x[n+1], y[n+1], f=0.00; 
     n = (int)round(x_end / h);
     x[0] = 0.0;
     y[0] = 1.0;
