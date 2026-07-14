b0834a13a52c9b6459d5ef4181b8795f077a36cd
Prodipta <nayebprodipta@gmail.com>
2026-07-14 11:24:02 +0530

    trying to write a clean code

--- Mathematical Programming/Eulers.c
+++ Mathematical Programming/Eulers.c
@@ -5,8 +5,8 @@ void main()
     int n=0,k=0;
     float h = 0.01, x0 = 0.0, y0 = 1.0, x_end = 0.02, x[n+1], y[n+1], f=0.00; 
     n = (int)round(x_end / h);
-    x[0] = x0;
-    y[0] = y0;
+    x[0] = 0.0;
+    y[0] = 1.0;
     for (k = 0; k < n; ++k)
     {
         f = pow(x[k], 3) + y[k];
