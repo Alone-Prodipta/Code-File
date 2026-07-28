f2361ce5d3c61121adc68ce1782cea9c577b0201
Prodipta <nayebprodipta@gmail.com>
2026-07-28 20:07:10 +0530

    doing some changes

--- Mathematical Programming/Bisection.c
+++ Mathematical Programming/Bisection.c
@@ -7,9 +7,11 @@ double func(double x)
 {
     return pow(e,x) - 3*x;
 }
+// for bijection method both values of a and b should be in opposite sign.
+// for regula falsy method both values of a and b should be in opposite sign. 
 void main() 
 {
-    double a = 1.5, b = 1.6 , c;   // initial interval [1,2]
+    double a = 1.5, b = -1.6 , c;   // initial interval [1,2]
     double tolerance = 0.0001; // stopping condition
     int maxIter = 100, iter = 0;
 
@@ -23,7 +25,7 @@ void main()
     printf("n\t a\t\t b\t\t c\t\t f(c)\n");
 
     //while (fabs(func(c)) >= tolerance && iter < maxIter)//regula falsy
-    while ((b - a) >= tolerance && iter < maxIter)//bisection
+    while (fabs(b - a) >= tolerance && iter < maxIter)//bisection
     {
         c = (a + b) / 2.0; // midpoint for bisection method
         //c = (a * func(b) - b * func(a)) / (func(b) - func(a));// fotrula of regula- falsy method
